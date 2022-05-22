fbda68c32048e5c98c19a6cf1a94224c9a5bd8c5
Romain <romain.magana@hotmail.com>
2022-05-22 23:57:20 +0200

    make it better

--- index.js
+++ index.js
@@ -3,38 +3,78 @@ const request = require('request');
 const cheerio = require('cheerio');
 const app     = express();
 
-app.get('/', function(req, res) {
+app.get('/', async function (req, res) {
 
     let url = req.query.url;
-    if (!url) {
-        res.send('No URL provided');
-        return;
+
+    if (isValidURL(url)) {
+            // decode URI url
+            let url2 = decodeURIComponent(url)
+
+            request(url2, async function (error, response, html) {
+                // First we'll check to make sure no errors occurred when making the request
+                if (!error) {
+                    let image = ' ';
+                    let title = ' ';
+                    let price = ' ';
+
+                    // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+                    let $ = cheerio.load(html);
+
+                    if (url2.includes('amazon')) {
+
+                        try {
+                            image = await $('#landingImage').attr('src');
+                        } catch (e) {
+                            image = 'No image found';
+                        }
+
+                        try {
+                            title = await $('#productTitle').text();
+                        } catch (e) {
+                            title = 'No title found';
+                        }
+
+                        try {
+                            price = await $('#corePrice_feature_div div span > span:first').text();
+                        } catch (e) {
+                            price = 'No price found';
+                        }
+
+                        // Configure the response to be in JSON format
+                        res.set({
+                            "Content-Type": "application/json",
+                            "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Credentials" : "true",
+                        });
+
+                        // And now, the JSON format we are going to expose
+                        let json = {
+                            image: image.trim(),
+                            title: title.trim(),
+                            price: price.trim()
+                        };
+
+                        // Send the JSON as a response to the client
+                        res.send(json);
+                    }else {
+                        res.send('This url is not supported');
+                    }
+                }
+            });
+    } else {
+        res.send('Bad Url');
     }
-    // decode URI url
-    let url2 = decodeURIComponent(url)
-    request(url2, function(error, response, html) {
-        // First we'll check to make sure no errors occurred when making the request
-        if (!error) {
 
-            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-            let $ = cheerio.load(html);
-
-            let image = $('#landingImage').attr('src');
-            let title = $('#productTitle').text();
-            let price = $('#corePrice_feature_div div span > span:first').text();
-
-            // And now, the JSON format we are going to expose
-            let json = {
-                image: image.trim(),
-                title: title.trim(),
-                price: price.trim()
-            };
-
-            // Send the JSON as a response to the client
-            res.send(json);
+    function isValidURL(string) {
+        if (string!= null) {
+            let res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+            return (res !== null)
         }
-
-    });
+        else {
+            return false;
+        }
+    }
 
 });
 
